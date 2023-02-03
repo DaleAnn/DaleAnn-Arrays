510c00b7dc7fc55fb6d9d0a8d77f2db675d3d1b7
DaleAnn <daviddaleann@yahoo.com>
2023-02-03 01:18:34 +0000

    Done!

--- src/main/java/ArrayBasics.java
+++ src/main/java/ArrayBasics.java
@@ -15,10 +15,9 @@ public class ArrayBasics {
      * @return the length of arr.
      */
     public int getLengthOfArray(int[] arr){
-        return 0;
+       
+        return arr.length;
     }
-
-
 
     /**
      * @param arr the array we're getting data from.
@@ -27,7 +26,7 @@ public class ArrayBasics {
      * @return the element at index n of arr.
      */
     public int getNthElementOfArray(int[] arr, int n){
-        return 0;
+        return arr[n];
     }
 
     /**
@@ -39,6 +38,7 @@ public class ArrayBasics {
      * @return nothing. Because of pass-by-reference, any change to the array will be reflected across our java program.
      */
     public void setNthElementOfArray(int[] arr, int n, int val){
+        arr[n] = val;
 
     }
 
@@ -47,6 +47,7 @@ public class ArrayBasics {
      * @return a new int array that can contain n elements.
      */
     public int[] returnNewArraySizeN(int n){
-        return null;
+        int arr[] = new int[n];
+        return arr;
     }
 }
